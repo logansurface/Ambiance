0645ed686d8bdf76f76534006e253d50e990ec1d
Logan Surface <surfacelogan@gmail.com>
2021-04-29 02:32:59 -0500

    Fixed border generation, displaying the border needs work now.

--- src/ambiance.py
+++ src/ambiance.py
@@ -7,6 +7,7 @@ import serial
 # A class for holding an array of color values, 
 # generated from the outer edges of an image
 class ColorFrame:
+    border = None
     height = 0
     width = 0
 
@@ -17,7 +18,7 @@ class ColorFrame:
         self.vres = vres
     
     def __str__(self):
-        return f"{self.width} x {self.height}"
+        return f"CDimensions: {self.width} x {self.height}\nColor Channels: {self.screen.shape[2]}"
     
     # Capture the screen and downscale it to avg. colors
     # and reduce computational intensity
@@ -28,13 +29,13 @@ class ColorFrame:
         self.width, self.height = self.width // scale_factor, self.height // scale_factor
 
         # Convert the raw image to a rescaled version and create a numpy array from this
-        self.screen = list(self.screen_raw.resize((self.width, self.height)).getdata())
-        self.screen = np.array(self.screen) #.reshape((self.width, self.height))
-        print(self.screen)
+        self.screen = self.screen_raw.resize((self.width, self.height)).getdata()
+        self.screen = np.array(self.screen, dtype=np.uint8).reshape(self.height, self.width, 3)
 
     # Calculates the average color of the pixels within bounding boxes, specified by the
     # screen dimensions and the resolution of the frame.
     def generate_border(self):
+        self.border = np.empty((self.height, self.width, 3), dtype=np.uint8)
         # Set the intervals and spacing as local variables to reduce number of operations
         interval_x = self.width // self.hres
         interval_y = self.height // self.vres
@@ -45,11 +46,12 @@ class ColorFrame:
             for x in range(self.hres):
                 px = self.screen[(int(y * interval_y) + spacing_y), int((x * interval_x) + spacing_x)]  
                 self.border[y][x] = px
+                print(f"[{y}][{x}]: {px}")
 
     # SHow the processed frame
     def show_frame(self):
-        disp = cv.cvtColor(self.screen, cv.COLOR_BGR2GRAY)
-        cv.imshow("Show Frame", disp)
+        #disp = cv.cvtColor(self.screen, cv.COLOR_BGR2GRAY)
+        cv.imshow("Show Frame", self.screen)
         cv.waitKey(0)
         cv.destroyAllWindows()
 
@@ -62,10 +64,8 @@ time = cv.getTickCount()
 frame = ColorFrame(16, 8)
 frame.capture_frame(4)
 frame.generate_border()
-print(frame.border.size)
+print(frame.border.ndim)
 
 print((cv.getTickCount() - time) / cv.getTickFrequency())
-print(f"frame: {frame}")
+print(frame)
 #frame.show_frame()
-
-#screen_np = np.array(capture_frame())
