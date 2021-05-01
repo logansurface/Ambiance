a14f5221c72b8ba796e8b335b08dcfca0433d1e8
Logan Surface <surfacelogan@gmail.com>
2021-05-01 00:31:22 -0500

    Border is displaying now, but the colors are slightly off.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Ambiance
- A python tool for creating ambient lighting for adressable led's based on the current screen state.
+ A python tool for creating ambient lighting via adressable led's based on the current screen state.
+
+## Hardware Needed
+- A computer
+- A microcontroller capable of running Arduino code (I used an ESP-32, a Teensy-LC would work fine too)
+- A Soldering Iron
+- Stranded Hookup Wire, 26-28 AWG is fine for our purposes
+- Solder (Using leaded over lead free will make it significantly easier to create reliable connections)
+- Wire Strippers, if your're not a masochist like myself
+- Around 5m of WS2811, WS2811b, or a WS2812 LED strips (based on a 50 inch television) 
+- A 5v power source, capable of delivering 1.5-2.0 Amps
+
+## Code Dependencies
+- Pillow (Image Capture)
+- OpenCV-Python (Image Processing)
+- Numpy (Fast N-Dimensional Arrays)
+- MatPlotLib (Displaying Data)
+- Serial (Sends color information to the microcontroller over USB)
--- src/ambiance.py
+++ src/ambiance.py
@@ -1,15 +1,15 @@
 from PIL import Image, ImageGrab
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 
 import serial
 
 '''
-A class for holding an array of color values, 
-generated from the outer edges of an image
+A class for holding an array of color values, generated from the outer edges of an image
 '''
 class ColorFrame:
-    border = None
+    border = False
     height = 0
     width = 0
 
@@ -23,8 +23,14 @@ class ColorFrame:
         self.vres = vres
     
     def __str__(self):
-        return f"Capture Dimensions: {self.width} x {self.height}\nBorder Dimensions: {self.border.shape}\nColor Channels: {self.screen.shape[2]}"
-    
+        obj_str = f"\n<ColorFrame Object @ {hex(id(self))}>\n"
+        obj_str += "-"*35 + "\n"
+        obj_str += f"Capture Dimensions: {self.width} x {self.height}\n"
+        obj_str += f"Border Shape: {self.border.shape}\n"
+        obj_str += f"Color Channels: {self.screen.shape[2]}"
+
+        return obj_str 
+
     '''
     Capture the screen and downscale it to avg. colors
     and reduce computational intensity
@@ -45,7 +51,7 @@ class ColorFrame:
     '''
     def generate_border(self):
         # Initialize a numpy list with size col:vres x row:hres x depth:3
-        # This list represents a BGR led array with user specified resolution
+        # This list represents a BGR led array with a user specified resolution
         self.border = np.empty((self.vres, self.hres, 3), dtype=np.uint8)
 
         # Set the intervals and spacing as local vars to reduce the number of operations
@@ -56,23 +62,64 @@ class ColorFrame:
 
         for y in range(self.vres):
             for x in range(self.hres):
-                px = self.screen[(int(y * interval_y) + spacing_y), int((x * interval_x) + spacing_x)]  
+                px = self.screen[(
+                                int((y * interval_y) + spacing_y),
+                                int((x * interval_x) + spacing_x)
+                                )]
                 self.border[y][x] = px
+        
+    '''
+    Displays a graphical representation of the border using MatPlotLib
+    '''
+    def show_border(self):
+        # Set the drawing area to be hres x vres
+        plt.axis([0, self.hres - 1, 0, self.vres - 1])
+        # Plot the top of the border
+        plt.scatter(np.arange(self.hres),
+                    np.zeros(self.hres)*(self.hres - 1),
+                    c=self.border[0, : , : ]*(1/255))
+        # Plot the bottom of the border
+        plt.scatter(np.arange(self.hres),
+                    np.zeros(self.hres),
+                    c=self.border[self.vres - 1, : , : ]*(1/255))
+        # Plot the left side of the border
+        plt.scatter(np.zeros(self.vres),
+                    np.arange(self.vres),
+                    c=self.border[ : , 0, : ]*(1/255))
+        # Plot the right side of the border
+        plt.scatter(np.zeros(self.vres)*(self.vres - 1),
+                    np.arange(self.vres),
+                    c=self.border[ : , self.vres - 1, : ]*(1/255))
+        plt.show()
+
+    '''
+    Returns a tuple representing the RGB value at the desired index
+    @param x - The column value to retrieve the color from 
+    @param y - The row value to retrieve the color from
+    '''
+    def get_border_color(self, x, y):
+        if self.border is False:
+            self.throw_not_processed()
+        
+        # The colors that were captured by OpenCV are stored as BGR
+        # We want to convert this to an RGB colorspace
+        bgr = self.border[y, x, : ]
+
+        # Return the rotated color values
+        return (bgr[2], bgr[1], bgr[0])
         
     '''
     Print out the ColorFrame in a human readable form 
     '''
-    def print_frame(self):
-        try:
-            print("Top Row:\n", self.border[0, ...], '\n')
-            print("Bot Bow:\n", self.border[self.vres - 1, ...], '\n')
-            print("Left Col:\n", self.border[1:self.vres - 1, 0, : ], '\n')
-            print("Right Col:\n", self.border[1:self.vres - 1, self.hres - 1, : ])
-        except TypeError:
-            print("Error, the current frame hasn't been generated yet.") 
-            print("Generate the frame using frame_obj.generate_border()")
-            exit(-1)
+    def print_border(self):
+        if self.border is False:
+            self.throw_not_processed()
 
+        print("Top:\n", self.border[0, ...], '\n')
+        print("Left:\n", self.border[1:self.vres - 1, 0, : ], '\n')
+        print("Right:\n", self.border[1:self.vres - 1, self.hres - 1, : ], '\n')
+        print("Bottom:\n", self.border[self.vres - 1, ...], '\n')
+    
     '''
     Show the processed capture
     '''
@@ -86,18 +133,34 @@ class ColorFrame:
     Show the original capture, before processing was applied
     '''
     def show_capture(self):
+        print(self.screen_raw.mode)
         self.screen_raw.show()
+    
+    '''
+    Throw this when the current frame has not been processed
+    It will display an appropriate error message to the user
+    and terminate program execution
+    '''
+    def throw_not_processed(self):
+        print("Error, the current frame's border hasn't been generated yet.") 
+        print("Generate the border using frame_obj.generate_border()")
+        exit(-1)
 
 # Run on program start
 time = cv.getTickCount()
 
 frame = ColorFrame(16, 8)
-frame.capture_frame()
+frame.capture_frame(1)
 frame.generate_border()
-delta_t = (cv.getTickCount() - time) / cv.getTickFrequency()
-frame.print_frame()
 
-print("Processing Time: ", delta_t , "Sec.")
-print("Frame Rate: ", (1 / delta_t), "FPS")
+delta_t = (cv.getTickCount() - time) / cv.getTickFrequency()
+
 print(frame, "\n")
+frame.show_border()
 frame.show_capture()
+
+frame.print_border()
+print("Processing Time: ", delta_t , "seconds")
+print("Frame Rate: ", round(1 / delta_t), "frames per second")
+print("Storage Type: ", frame.border.dtype)
+print("-"*35 + "\n\n")
